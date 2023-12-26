2462466fb3f269c3bc74e5d0bf3c6f1b6a5ea6a3
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-26 21:52:22 +0100

    Refactor the router with apiResource

--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -1,14 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'users'], function(){
 
-    Route::get('/', [UserController::class, 'index']);
-    Route::get('/{USER}', [UserController::class, 'show']);
-    Route::post('/', [UserController::class, 'store']);
-    Route::put('/', [UserController::class, 'update']);
-    Route::delete('/{USER}', [UserController::class, 'destroy']);
-});
+Route::apiResource('users',UserController::class);
