9c021021b7efcf9bf056ee0840f454732a6b4538
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-26 20:30:06 +0100

    Create user routes

--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::group(['prefix' => 'users'], function(){
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{USER}', [UserController::class, 'show']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::put('/', [UserController::class, 'update']);
+    Route::delete('/{USER}', [UserController::class, 'destroy']);
 });
