9df2c5b630f906f90f78da8ca5e2f62fa1bfe4a7
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-28 21:21:47 +0100

    Create authcontroller and login method

---
+++ laravel-admin/app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $user = Auth::user();
+            $token = $user->createToken('admin')->accessToken;
+
+            return [
+                'token' => $token
+            ];
+        }
+
+        return response([
+            'message' => 'invalid credentials'
+        ],Response::HTTP_UNAUTHORIZED);
+    }
+}
