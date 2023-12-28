d2ec9410012932cd68a1386b8c54b0dcd474c970
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-28 21:23:14 +0100

    Add login route to api router

--- laravel-admin/app/Providers/AuthServiceProvider.php
+++ laravel-admin/app/Providers/AuthServiceProvider.php
@@ -22,7 +22,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-    Route::passport();
+    
 
     }
 }
--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::post('/login', [AuthController::class, 'login']);
 Route::apiResource('users',UserController::class);
