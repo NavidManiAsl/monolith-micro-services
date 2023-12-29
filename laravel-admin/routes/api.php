bdd677aee0fcbbbfb7145e3d671cd5d91a2ab2c5
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-29 19:33:57 +0100

    Add register route and login get

--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -5,5 +5,12 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::apiResource('users',UserController::class);
+Route::get('login', function(){
+    return response(['Error' => 'Unauthorized'],401);
+});
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register']);
+
+
+
+Route::apiResource('users',UserController::class)->middleware('auth:api');
