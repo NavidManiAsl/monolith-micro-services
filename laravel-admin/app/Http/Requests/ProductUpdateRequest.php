5ff4b569a09fc4ab468463086cca80e5b39ecba2
NavidManiAsl <navid.maniasl@outlook.com>
2024-01-07 21:26:45 +0100

    Create validation for user input on update method

---
+++ laravel-admin/app/Http/Requests/ProductUpdateRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => [
+
+                'min:3',
+                'max:256'
+            ],
+            'description' => [
+
+                'min:3',
+                'max:1024'
+            ],
+            'image' => ['url'],
+            'price' => ['numeric']
+        ];
+    }
+}
